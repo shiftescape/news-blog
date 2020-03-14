071a2bab8d37cb26b8b8de7cc7a6712ba63ccfd5
Alvin James Bellero <alvin.bellero@theredx.com>
2020-03-14 18:12:00 +0800

    Implemented news list

--- client/app/controllers/NewsCtrl.js
+++ client/app/controllers/NewsCtrl.js
@@ -2,11 +2,23 @@
 
 angular.module('myApp')
   .controller('NewsCtrl', function ($scope, NewsService) {
+    $scope.isEmptyNews = false;
+    $scope.isErrorNews = false;
+    $scope.allNews = [];
+
+    $scope.parseDate = function(date) {
+      return new Date(date).toString();
+    }
+
     NewsService.getNews()
       .then(function(response) {
-        console.log(response);
+        var response = response.data;
+        $scope.isEmptyNews = response.data.results < 1;
+        $scope.allNews = response.data.values.filter((news) => news.title && news.content);
+        console.log($scope.allNews);
       })
       .catch(function(err) {
+        $scope.isErrorNews = true;
         console.error(err);
       })
   });
